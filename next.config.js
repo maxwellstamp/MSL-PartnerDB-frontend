9f2e18b1b2c70a1006b17fdc4b6883ac16b31756
sakibfootball <sakibhaider20@gmail.com>
2025-10-29 12:15:04 +0600

    Fix: Replaced the new backend link with the previous one

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export", // ensures static export
+  output: "export", // ensures static export
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
